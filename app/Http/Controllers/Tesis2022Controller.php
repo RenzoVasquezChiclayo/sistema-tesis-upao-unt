db3d3fbfffdce0e315b9fb541771bce45058dc6b
jairorr <jairo.2aldair@gmail.com>
2023-10-05 12:51:53 -0500

    fixed error while adding images

--- app/Http/Controllers/Tesis2022Controller.php
+++ app/Http/Controllers/Tesis2022Controller.php
@@ -267,18 +267,20 @@ class Tesis2022Controller extends Controller
                             if (!file_exists($destinationPath)) {
                                 mkdir($destinationPath,0777,true);
                             }
-                            for($j = 0; $j<sizeof($file);$j++){
-                                $posImg = $j;
-                                $listDetalle = Detalle_Archivo::where('cod_archivos',$historialArchivos->cod_archivos)->where('grupo',$i)->where('tipo','resultados')->get();
-                                if(sizeof($listDetalle)>0) $posImg = sizeof($listDetalle);
-                                $det_archivo = new Detalle_Archivo();
-                                $filename = $i.$posImg.'-'.$tesis->id_grupo_inves.'.jpg';
-                                $uploadSuccess = $file[$j]->move($destinationPath,$filename);
-                                $det_archivo->cod_archivos = $historialArchivos->cod_archivos;
-                                $det_archivo->tipo = 'resultados';
-                                $det_archivo->ruta = $filename;
-                                $det_archivo->grupo = $i;
-                                $det_archivo->save();
+                            if($file!=null){
+                                for($j = 0; $j<sizeof($file);$j++){
+                                    $posImg = $j;
+                                    $listDetalle = Detalle_Archivo::where('cod_archivos',$historialArchivos->cod_archivos)->where('grupo',$i)->where('tipo','resultados')->get();
+                                    if(sizeof($listDetalle)>0) $posImg = sizeof($listDetalle);
+                                    $det_archivo = new Detalle_Archivo();
+                                    $filename = $i.$posImg.'-'.$tesis->id_grupo_inves.'.jpg';
+                                    $uploadSuccess = $file[$j]->move($destinationPath,$filename);
+                                    $det_archivo->cod_archivos = $historialArchivos->cod_archivos;
+                                    $det_archivo->tipo = 'resultados';
+                                    $det_archivo->ruta = $filename;
+                                    $det_archivo->grupo = $i;
+                                    $det_archivo->save();
+                                }
                             }
                         }
                     }
@@ -292,17 +294,19 @@ class Tesis2022Controller extends Controller
                     if (!file_exists($destinationPath)) {
                         mkdir($destinationPath,0777,true);
                     }
-                    for($j = 0; $j<sizeof($file);$j++){
-                        $posImg = $j;
-                        $listDetalle = Detalle_Archivo::where('cod_archivos',$historialArchivos->cod_archivos)->where('grupo','0')->where('tipo','resultados')->get();
-                        if(sizeof($listDetalle)>0) $posImg = sizeof($listDetalle);
-                        $filename = '0'.$posImg.'-'.$tesis->id_grupo_inves.'.jpg';
-                        $uploadSuccess = $file[$j]->move($destinationPath,$filename);
-                        $det_archivo->cod_archivos = $historialArchivos->cod_archivos;
-                        $det_archivo->tipo = 'resultados';
-                        $det_archivo->ruta = $filename;
-                        $det_archivo->grupo = 0;
-                        $det_archivo->save();
+                    if($file!=null){
+                        for($j = 0; $j<sizeof($file);$j++){
+                            $posImg = $j;
+                            $listDetalle = Detalle_Archivo::where('cod_archivos',$historialArchivos->cod_archivos)->where('grupo','0')->where('tipo','resultados')->get();
+                            if(sizeof($listDetalle)>0) $posImg = sizeof($listDetalle);
+                            $filename = '0'.$posImg.'-'.$tesis->id_grupo_inves.'.jpg';
+                            $uploadSuccess = $file[$j]->move($destinationPath,$filename);
+                            $det_archivo->cod_archivos = $historialArchivos->cod_archivos;
+                            $det_archivo->tipo = 'resultados';
+                            $det_archivo->ruta = $filename;
+                            $det_archivo->grupo = 0;
+                            $det_archivo->save();
+                        }
                     }
                 }
             }
@@ -334,18 +338,20 @@ class Tesis2022Controller extends Controller
                             if (!file_exists($destinationPath)) {
                                 mkdir($destinationPath,0777,true);
                             }
-                            for($j = 0; $j<sizeof($file);$j++){
-                                $posImg = $j;
-                                $listDetalle = Detalle_Archivo::where('cod_archivos',$historialArchivos->cod_archivos)->where('grupo',$i)->where('tipo','anexos')->get();
-                                if(sizeof($listDetalle)>0) $posImg = sizeof($listDetalle);
-                                $det_archivo = new Detalle_Archivo();
-                                $filename = $i.$posImg.'-'.$tesis->id_grupo_inves.'.jpg';
-                                $uploadSuccess = $file[$j]->move($destinationPath,$filename);
-                                $det_archivo->cod_archivos = $historialArchivos->cod_archivos;
-                                $det_archivo->tipo = 'anexos';
-                                $det_archivo->ruta = $filename;
-                                $det_archivo->grupo = $i;
-                                $det_archivo->save();
+                            if($file!=null){
+                                for($j = 0; $j<sizeof($file);$j++){
+                                    $posImg = $j;
+                                    $listDetalle = Detalle_Archivo::where('cod_archivos',$historialArchivos->cod_archivos)->where('grupo',$i)->where('tipo','anexos')->get();
+                                    if(sizeof($listDetalle)>0) $posImg = sizeof($listDetalle);
+                                    $det_archivo = new Detalle_Archivo();
+                                    $filename = $i.$posImg.'-'.$tesis->id_grupo_inves.'.jpg';
+                                    $uploadSuccess = $file[$j]->move($destinationPath,$filename);
+                                    $det_archivo->cod_archivos = $historialArchivos->cod_archivos;
+                                    $det_archivo->tipo = 'anexos';
+                                    $det_archivo->ruta = $filename;
+                                    $det_archivo->grupo = $i;
+                                    $det_archivo->save();
+                                }
                             }
                         }
                     }
@@ -359,17 +365,19 @@ class Tesis2022Controller extends Controller
                     if (!file_exists($destinationPath)) {
                         mkdir($destinationPath,0777,true);
                     }
-                    for($j = 0; $j<sizeof($file);$j++){
-                        $posImg = $j;
-                        $listDetalle = Detalle_Archivo::where('cod_archivos',$historialArchivos->cod_archivos)->where('grupo','0')->where('tipo','anexos')->get();
-                        if(sizeof($listDetalle)>0) $posImg = sizeof($listDetalle);
-                        $filename = '0'.$posImg.'-'.$tesis->id_grupo_inves.'.jpg';
-                        $uploadSuccess = $file[$j]->move($destinationPath,$filename);
-                        $det_archivo->cod_archivos = $historialArchivos->cod_archivos;
-                        $det_archivo->tipo = 'anexos';
-                        $det_archivo->ruta = $filename;
-                        $det_archivo->grupo = 0;
-                        $det_archivo->save();
+                    if($file!=null){
+                        for($j = 0; $j<sizeof($file);$j++){
+                            $posImg = $j;
+                            $listDetalle = Detalle_Archivo::where('cod_archivos',$historialArchivos->cod_archivos)->where('grupo','0')->where('tipo','anexos')->get();
+                            if(sizeof($listDetalle)>0) $posImg = sizeof($listDetalle);
+                            $filename = '0'.$posImg.'-'.$tesis->id_grupo_inves.'.jpg';
+                            $uploadSuccess = $file[$j]->move($destinationPath,$filename);
+                            $det_archivo->cod_archivos = $historialArchivos->cod_archivos;
+                            $det_archivo->tipo = 'anexos';
+                            $det_archivo->ruta = $filename;
+                            $det_archivo->grupo = 0;
+                            $det_archivo->save();
+                        }
                     }
                 }
             }
